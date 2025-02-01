12c4068a3964ca9ea6309767c9e8eb41fa61c594
GitHub Action <action@github.com>
2025-02-01 18:35:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/tobasqo/githubdarkforpython/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/tobasqo/githubdarkforpython/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tobasqo.githubdarkforpython.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
