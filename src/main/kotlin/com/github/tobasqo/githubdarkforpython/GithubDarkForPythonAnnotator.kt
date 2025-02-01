80efd10894c7e38644c5169d34bde2a1b09088bf
tobasqo <szymon.j.jaskowski@gmail.com>
2025-02-01 23:12:21 +0100

    function references arent colored as instances

--- src/main/kotlin/com/github/tobasqo/githubdarkforpython/GithubDarkForPythonAnnotator.kt
+++ src/main/kotlin/com/github/tobasqo/githubdarkforpython/GithubDarkForPythonAnnotator.kt
@@ -7,16 +7,17 @@ import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.jetbrains.python.psi.PyReferenceExpression
+import com.jetbrains.python.psi.*
 import org.jetbrains.annotations.NotNull
 
 class GithubDarkForPythonAnnotator : Annotator {
     override fun annotate(@NotNull element: PsiElement, @NotNull holder: AnnotationHolder) {
         val isLeaf = element is LeafPsiElement
-        val isInstanceField = element.parent is PyReferenceExpression
         val isNotInstanceItself = element.text != element.parent.text
+        val isNotCall = element.parent.parent !is PyCallExpression
+        val isInstanceField = element.parent is PyReferenceExpression
 
-        if (isLeaf && isInstanceField && isNotInstanceItself) {
+        if (isLeaf && isNotCall && isNotInstanceItself && isInstanceField) {
             val range = TextRange(element.textRange.startOffset, element.textRange.endOffset)
             holder
                 .newSilentAnnotation(HighlightSeverity.INFORMATION)
